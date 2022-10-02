c73808ceb3ba1cbdfdc9375d8517fc293dee2084
maqamylee0 <maqamylee0@gmail.com>
2022-10-02 11:00:54 +0300

    Initial commit

---
+++ app/src/main/java/com/tech4dev/wtflogin/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.tech4dev.wtflogin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
